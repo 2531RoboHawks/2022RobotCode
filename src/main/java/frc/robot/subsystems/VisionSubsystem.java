ade2de75a3c5e57690673edb665357018ad04b43
sharedcomputer <95010825+sharedcomputer@users.noreply.github.com>
2022-02-16 19:07:51 -0600

    Flesh out vision subsystem with hypothetical code

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,17 +1,40 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionSubsystem extends SubsystemBase {
-    /* 
-    TODO: 
-    Distance Equation
-    */
-    public VisionSubsystem() {}
+    // https://docs.limelightvision.io/en/latest/networktables_api.html
+    private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    private NetworkTableEntry tv = table.getEntry("tv");
+    private NetworkTableEntry tx = table.getEntry("tx");
+    private NetworkTableEntry ty = table.getEntry("ty");
+    private NetworkTableEntry ta = table.getEntry("ta");
 
-    public void stop() {}
+    public VisionSubsystem() {
+
+    }
+
+    public boolean hasValidTarget() {
+        return tv.getDouble(0) == 1;
+    }
+
+    public double getX() {
+        return tx.getDouble(0);
+    }
+
+    public double getY() {
+        return ty.getDouble(0);
+    }
+
+    public double getArea() {
+        return ta.getDouble(0);
+    }
 
     @Override
-    public void periodic() {}
+    public void periodic() {
 
+    }
 }
