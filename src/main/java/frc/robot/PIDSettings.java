19d3e10ea9be10fd8c1d3874fcdd2374b7d69e5d
sharedcomputer <95010825+sharedcomputer@users.noreply.github.com>
2022-03-11 17:55:55 -0600

    Add various helper methods

--- src/main/java/frc/robot/PIDSettings.java
+++ src/main/java/frc/robot/PIDSettings.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.math.controller.PIDController;
+
 public class PIDSettings {
     public final double kp;
     public final double ki;
@@ -13,5 +15,9 @@ public class PIDSettings {
         this.kp = kp;
         this.ki = ki;
         this.kd = kd;
+    }
+
+    public PIDController toController() {
+        return new PIDController(kp, ki, kd);
     }
 }
