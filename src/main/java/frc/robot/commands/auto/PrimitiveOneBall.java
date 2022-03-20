3ee472bcf52cbef85d25ffcba2f5e4a521755429
sharedcomputer <95010825+sharedcomputer@users.noreply.github.com>
2022-03-20 12:46:24 -0500

    Remove unused import

--- src/main/java/frc/robot/commands/auto/PrimitiveOneBall.java
+++ src/main/java/frc/robot/commands/auto/PrimitiveOneBall.java
@@ -2,7 +2,6 @@ package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShootSubsystem;
 
 public class PrimitiveOneBall extends SequentialCommandGroup {
