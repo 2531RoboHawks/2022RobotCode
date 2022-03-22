c93cf38e253061dc52da3add444d35f08da3d318
sharedcomputer <95010825+sharedcomputer@users.noreply.github.com>
2022-03-22 16:25:03 -0500

    Use WPILib's conversion utility instead

--- src/main/java/frc/robot/commands/auto/Waypoint.java
+++ src/main/java/frc/robot/commands/auto/Waypoint.java
@@ -2,7 +2,7 @@ package frc.robot.commands.auto;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import frc.robot.Convert;
+import edu.wpi.first.math.util.Units;
 
 public class Waypoint {
   // All units are in meters or degrees
@@ -20,7 +20,7 @@ public class Waypoint {
   }
 
   public static Waypoint inches(double x, double y, double rotation) {
-    return new Waypoint(Convert.inchToMeter(x), Convert.inchToMeter(y), rotation);
+    return new Waypoint(Units.inchesToMeters(x), Units.inchesToMeters(y), rotation);
   }
 
   public Pose2d getPose() {
