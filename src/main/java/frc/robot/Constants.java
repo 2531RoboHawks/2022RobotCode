9304846d379a4c3c72147d38aae7d72f73e0a120
sharedcomputer <95010825+sharedcomputer@users.noreply.github.com>
2022-03-24 12:31:53 -0500

    Tune PID!

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,8 +23,8 @@ public final class Constants {
 
   public static final class RotatePID {
     // (in degrees)
-    public static final double kP = 0.013;
-    public static final double kI = 0.01;
+    public static final double kP = 0.0115;
+    public static final double kI = 0.012;
     public static final double kD = 0;
   }
 
