7b484d914273fb444af78117500622dc14f60f7b
sharedcomputer <95010825+sharedcomputer@users.noreply.github.com>
2022-03-23 20:52:58 -0500

    Fixup intake requirement issues

--- src/main/java/frc/robot/commands/auto/TheRumbling.java
+++ src/main/java/frc/robot/commands/auto/TheRumbling.java
@@ -1,16 +1,14 @@
 package frc.robot.commands.auto;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.RunIntakeCommandGroup;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShootSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
 
 public class TheRumbling extends SequentialCommandGroup {
-  private IntakeSubsystem intakeSubsystem;
-
   private static final Waypoint START = new Waypoint(7.71, 2.75, -90.00);
   private static final Waypoint FIRST_BALL = new Waypoint(7.71, 0.5, -90.00);
   private static final Waypoint SECOND_BALL = new Waypoint(5.0, 1.98, -90.00);
@@ -18,30 +16,18 @@ public class TheRumbling extends SequentialCommandGroup {
   private static final Waypoint FINAL_SHOT = SECOND_BALL;
 
   public TheRumbling(DriveSubsystem driveSubsystem, IntakeSubsystem intakeSubsystem, ShootSubsystem shootSubsystem, VisionSubsystem visionSubsystem) {
-    this.intakeSubsystem = intakeSubsystem;
-    addRequirements(driveSubsystem, intakeSubsystem, shootSubsystem);
     addCommands(new ResetOdometryCommand(driveSubsystem, START));
     addCommands(new AutoShootCommand(shootSubsystem, visionSubsystem, intakeSubsystem));
-    addCommands(new InstantCommand(() -> {
-      intakeSubsystem.setDown(true);
-      intakeSubsystem.setSpinning(true);
-    }));
-    addCommands(TrajectoryCommand.fromWaypoints(driveSubsystem, START, FIRST_BALL));
-    addCommands(new WaitCommand(1));
-    addCommands(TrajectoryCommand.fromWaypoints(driveSubsystem, FIRST_BALL, SECOND_BALL));
-    // TODO: shoot two balls from here
-    addCommands(new WaitCommand(1));
-    addCommands(TrajectoryCommand.fromWaypoints(driveSubsystem, SECOND_BALL, TERMINAL));
-    addCommands(new WaitCommand(1));
-    addCommands(TrajectoryCommand.fromWaypoints(driveSubsystem, TERMINAL, FINAL_SHOT));
-    // TODO: shoot two balls from here
-    addCommands(new WaitCommand(1));
-  }
-
-  @Override
-  public void end(boolean interrupted) {
-    super.end(interrupted);
-    intakeSubsystem.setSpinning(false);
-    intakeSubsystem.setDown(false);
+    addCommands(new RunIntakeCommandGroup(
+      intakeSubsystem,
+      TrajectoryCommand.fromWaypoints(driveSubsystem, START, FIRST_BALL),
+      new WaitCommand(1),
+      TrajectoryCommand.fromWaypoints(driveSubsystem, FIRST_BALL, SECOND_BALL),
+      new WaitCommand(1),
+      TrajectoryCommand.fromWaypoints(driveSubsystem, SECOND_BALL, TERMINAL),
+      new WaitCommand(1),
+      TrajectoryCommand.fromWaypoints(driveSubsystem, TERMINAL, FINAL_SHOT),
+      new WaitCommand(1)
+    ));
   }
 }
--- src/main/java/frc/robot/commands/auto/TwoBallAuto.java
+++ src/main/java/frc/robot/commands/auto/TwoBallAuto.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.RunIntakeCommandGroup;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShootSubsystem;
@@ -13,11 +14,11 @@ public class TwoBallAuto extends SequentialCommandGroup {
   public TwoBallAuto(DriveSubsystem driveSubsystem, ShootSubsystem shootSubsystem, IntakeSubsystem intakeSubsystem, VisionSubsystem visionSubsystem) {
     addCommands(new ResetOdometryCommand(driveSubsystem));
     addCommands(new ShootOneBall(shootSubsystem, intakeSubsystem, visionSubsystem));
-    addCommands(new InstantCommand(() -> {
-      intakeSubsystem.setEverything(true);
-    }, intakeSubsystem));
-    addCommands(new DriveToWaypoint(driveSubsystem, Units.inchesToMeters(42.421), 0, 0));
-    addCommands(new WaitCommand(2));
-    addCommands(new ShootOneBall(shootSubsystem, intakeSubsystem, visionSubsystem));
+    addCommands(new RunIntakeCommandGroup(
+      intakeSubsystem,
+      new DriveToWaypoint(driveSubsystem, Units.inchesToMeters(42.421), 0, 0),
+      new WaitCommand(2),
+      new ShootOneBall(shootSubsystem, intakeSubsystem, visionSubsystem)
+    ));
   }
 }
