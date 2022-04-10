3f2f125ec778bf53b126e195098ec28ec391f8c0
sharedcomputer <95010825+sharedcomputer@users.noreply.github.com>
2022-04-10 18:10:10 -0500

    Remove unused import

--- src/main/java/frc/robot/commands/defaults/DefaultDrive.java
+++ src/main/java/frc/robot/commands/defaults/DefaultDrive.java
@@ -4,7 +4,6 @@
 
 package frc.robot.commands.defaults;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.InputUtils;
 import frc.robot.RobotContainer;
