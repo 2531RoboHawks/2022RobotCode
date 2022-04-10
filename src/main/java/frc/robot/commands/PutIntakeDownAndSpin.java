739ad28762bd2e1eeefae26d9209459e22fadbce
sharedcomputer <95010825+sharedcomputer@users.noreply.github.com>
2022-04-10 17:37:11 -0500

    Repeatedly call setEverything when intake down

--- src/main/java/frc/robot/commands/PutIntakeDownAndSpin.java
+++ src/main/java/frc/robot/commands/PutIntakeDownAndSpin.java
@@ -1,18 +1,28 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.IntakeSubsystem;
 
-public class PutIntakeDownAndSpin extends StartEndCommand {
+public class PutIntakeDownAndSpin extends CommandBase {
+  private IntakeSubsystem intakeSubsystem;
+
   public PutIntakeDownAndSpin(IntakeSubsystem intakeSubsystem) {
-    super(
-      () -> {
-        intakeSubsystem.setEverything(true);
-      },
-      () -> {
-        intakeSubsystem.setEverything(false);
-      },
-      intakeSubsystem
-    );
+    addRequirements(intakeSubsystem);
+    this.intakeSubsystem = intakeSubsystem;
+  }
+
+  @Override
+  public void initialize() {
+    intakeSubsystem.setEverything(true);
+  }
+
+  @Override
+  public void execute() {
+    intakeSubsystem.setEverything(true);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    intakeSubsystem.setEverything(false);
   }
 }
